295d92bcfff94e31ce520e36c1ec433466df4386
KatPertuz <katherin.o.pertuz@gmail.com>
2026-08-24 21:37:37 -0500

    variables bases

---
+++ VentasIterativos.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class VentasIterativos {
+
+    public static void main(String[] args) {
+        
+        Scanner leer = new Scanner(System.in);
+        int opcion;
+        double valor, porcentajePremium, totalBruto, descuento, totalSinIva;
+        double totalReal, totalSindescuento;
+        int cantidadBasico, cantidadMedio, cantidadPremiun, totalcantidad;
+        double PORCENTAJE_DESCUENTO= 0.1;
+        double IVA=0.19;
+        double COMPRA_APLICABLE=100000;
+        double PORCIENTO=100;
+
+       
+
+
+
+
+        leer.close();
+    }
+
+    
+}
